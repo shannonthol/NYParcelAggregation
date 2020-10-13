23f77259e8c65f643cfc27bc0f22b6026a0efcf7
Shannon Thol <37225138+shannonthol@users.noreply.github.com>
2020-10-13 09:13:02 -0400

    Update owner_addr_sets.sql
    updated for bigquery

--- owner_addr_sets.sql
+++ owner_addr_sets.sql
@@ -1,26 +1,36 @@
-create table parcels.stdown_set as
-
+--code for creating a table of standard owner names with unique ids (uuids) for parcel centroids that have a non-null standard owner names
+--destination table ny-state-parcel-ownership:tnc_ny_parcels.rps_stdown_set
 select 
-string_agg(distinct cast(id as varchar), ', ') as ids,
-std_owner, 
-string_agg(distinct cast(owner_type as varchar), ', ') as own_types,
-round(sum(calc_acres::float)::float,2) as sum_acres, 
-count(id) as num
-from parcels.rpscentroids
-where std_owner != ''
-group by std_owner
-order by num desc
+  generate_uuid() as uuid,
+  string_agg(distinct cast(id as string), ", ") as ids,
+  std_owner,
+  string_agg(distinct cast(owner_type as string), ", ") as own_types,
+  sum(calc_acres) as sum_acres,
+  count(id) as num
+from 
+  tnc_ny_parcels.rpscentroids_all
+where 
+  std_owner != ''
+group by 
+  std_owner
+order by 
+  num desc
 
 
-create table parcels.stdaddr_set as
-
+--code for creating a table of standard owner addresses with unique ids (uuids) for parcel centroids that have a non-null standard address
+--destination table ny-state-parcel-ownership:tnc_ny_parcels.rps_stdaddr_set
 select 
-string_agg(distinct cast(id as varchar), ', ') as ids,
-std_addr, 
-string_agg(distinct cast(owner_type as varchar), ', ') as own_types,
-round(sum(calc_acres)::numeric,2) as sum_acres, 
-count(id) as num
-from parcels.rpscentroids
-where std_addr != ''
-group by std_addr
-order by num desc
+  generate_uuid() as uuid,
+  string_agg(distinct cast(id as string), ", ") as ids,
+  std_addr,
+  string_agg(distinct cast(owner_type as string), ", ") as own_types,
+  sum(calc_acres) as sum_acres,
+  count(id) as num
+from 
+  tnc_ny_parcels.rpscentroids_all
+where 
+  std_addr != ''
+group by 
+  std_addr
+order by 
+  num desc
