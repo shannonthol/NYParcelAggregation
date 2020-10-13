620ede45448073251f4571328631e19a13fcedaa
Shannon Thol <37225138+shannonthol@users.noreply.github.com>
2020-10-13 09:34:06 -0400

    Update owner_addr_sets.sql
    added comments to explain fields

--- owner_addr_sets.sql
+++ owner_addr_sets.sql
@@ -1,5 +1,12 @@
 --code for creating a table of standard owner names with unique ids (uuids) for parcel centroids that have a non-null standard owner names
 --destination table ny-state-parcel-ownership:tnc_ny_parcels.rps_stdown_set
+--FIELDS: 
+--uuid = unique id for standard owner name set
+--ids = comma separated list of ids from rpscentroids_all with the specified standardized owner name
+--std_owner = standardized owner name from rpscentroids_all
+--own_types = comma separated list of owner types with the specified standardized owner name
+--sum_acres = sum of calculated areas field from rpscentroids_all for all records with the specified standardized owner name
+--num = number of records from rpscentroids_all with the specified standardized owner name
 select 
   generate_uuid() as uuid,
   string_agg(distinct cast(id as string), ", ") as ids,
@@ -19,6 +26,13 @@ order by
 
 --code for creating a table of standard owner addresses with unique ids (uuids) for parcel centroids that have a non-null standard address
 --destination table ny-state-parcel-ownership:tnc_ny_parcels.rps_stdaddr_set
+--FIELDS: 
+--uuid = unique id for standard owner name set
+--ids = comma separated list of ids from rpscentroids_all with the specified standardized owner address
+--std_addr = standardized owner address from rpscentroids_all
+--own_types = comma separated list of owner types with the specified standardized owner address
+--sum_acres = sum of calculated areas field from rpscentroids_all for all records with the specified standardized owner address
+--num = number of records from rpscentroids_all with the specified standardized owner address
 select 
   generate_uuid() as uuid,
   string_agg(distinct cast(id as string), ", ") as ids,
