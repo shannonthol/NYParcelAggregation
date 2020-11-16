a51ce55fed44c5fb2fbd6e37aa04b3c8a4bec088
Shannon Thol <37225138+shannonthol@users.noreply.github.com>
2020-11-16 11:34:21 -0500

    Update owner_addr_sets.sql
    added code for combined standard owner name and address sets

--- owner_addr_sets.sql
+++ owner_addr_sets.sql
@@ -48,3 +48,19 @@ group by
   std_addr
 order by 
   num desc
+  
+--code for creating a table of standard owner names and address sets for parcel centroids  
+with foo as
+(select 
+  id, std_owner, std_addr, concat(std_owner, " ", std_addr) as own_add, calc_acres 
+from 
+  tnc_ny_parcels.rpscentroids_all 
+  )
+select 
+  row_number() over() as setid, count(id) as num_ids, length(std_owner) as own_strnum, length(std_addr) as add_strnum, sum(calc_acres) as sum_acres, string_agg(distinct cast(id as string), ", ") as ids, std_owner, std_addr, own_add 
+from 
+  foo 
+group by 
+  own_add, std_owner, std_addr 
+order by 
+  num_ids desc
