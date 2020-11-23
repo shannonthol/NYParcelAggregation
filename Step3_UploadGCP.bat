1dd6420dd8b5ea30ee5bb24591135953260df0a6
Shannon Thol <37225138+shannonthol@users.noreply.github.com>
2020-11-23 11:24:56 -0500

    Create Step3_UploadGCP.bat

---
+++ Step3_UploadGCP.bat
@@ -0,0 +1,24 @@
+--This code (step 3) loads the batch csv files into our Google Cloud Platform Bucket
+
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch1.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch2.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch3.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch4.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch5.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch6.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch7.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch8.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch9.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch10.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch11.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch12.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch13.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch14.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch15.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch16.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch17.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch18.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch19.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch20.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch21.csv gs://tnc-eny-bucket01/rpscentroids
+call gsutil cp C:\Users\shannon.thol\Documents\ArcGIS\Projects\ParcelAggregation\NonadjacentAggregation\dev\BigQuery\rpscentroids_batch22.csv gs://tnc-eny-bucket01/rpscentroids
