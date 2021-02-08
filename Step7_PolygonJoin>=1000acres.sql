515cc1ae2db408cc2bd8ae22cb1ddff4d027237e
Shannon Thol <37225138+shannonthol@users.noreply.github.com>
2021-02-08 10:40:50 -0500

    Update and rename Step7_.sql to Step7_PolygonJoin>=1000acres.sql

--- Step7_PolygonJoin>=1000acres.sql
+++ Step7_PolygonJoin>=1000acres.sql
@@ -1,5 +1,6 @@
 
 --select polygons that contain the centroids of ownership collections with total area greater than or equal to 1000 acres
+create table tnc_ny_parcels.polyscontain_rpscentroids_collectiondata_gtet1000acres
 select polys.gid, polys.parcel_id,
 pts.parcid, pts.setid, pts.collid, pts.std_own, pts.std_add, pts.coll_own, pts.coll_add, pts.parc_acres, pts.coll_acres, pts.coll_numparcs,
 polys.geography
